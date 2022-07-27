6eed472cf483528c2f17dd74e5eb52278368dd1f
Steve Falkovsky <37025050+WereWolf232@users.noreply.github.com>
2022-07-27 11:45:54 +0300

    Update project.py

--- project.py
+++ project.py
@@ -2,53 +2,37 @@ from pydub import AudioSegment
 import librosa
 import soundfile
 import os
+from tkinter import *
+from tkinter import filedialog
 
 
-#choose whether to do simple harmony or canon
+# program allows user to either create a harmony or a canon for a given audio file
+
+harmony_canon = ""
 
 def main():
-    # User picks file
+    sound1 = openfile() 
+    #request input from user whether he wants harmony or canon
     while True:
         try:
-            sound1 = AudioSegment.from_file(input("choose .wav file: "), format="wav")
-            break
-        except (FileNotFoundError):
-            print("file not found")
-
-    # request input from user whether he wants harmony or canon
-    while True:
-        try:
+            global harmony_canon
             harmony_canon = input("harmony or canon? ")
-            if harmony_canon not in ['h','H','c','C']:
-                raise ValueError
+            if harmony_canon not in ['h','H','c','C']: raise ValueError    
             break
-        except ValueError:
-            pass
-
+        except ValueError: print("h/c")
+    
 
     harmony = harmonize(sound1)
     panned = pan(*harmony)
     if harmony_canon in ['h','H']:      #only harmonize (without delay)
         harmony = overlay(*panned)
-        # Let user choose what name the file should be saved as
-        while True:
-            try:
-                harmony.export("audio files/harmony/" + input("save as: ") + ".wav", format="wav")   #output harmony
-                break
-            except (FileNotFoundError):
-                print("File name invalid")
+        savefile(harmony)                # save as...
         os.remove("sound2.wav")
 
-    else:                               #implement delayed harmony
+    else:                               #implement delayed harmony 
         delayed = delay(panned[1])
         canon = overlay(delayed, panned[0])
-        # Let user choose what name the file should be saved as
-        while True:
-            try:
-                canon.export("audio files/canon/" + input("save as: ") + ".wav", format="wav")   #output canon
-                break
-            except (FileNotFoundError):
-                print("File name invalid")
+        savefile(canon)                  # save as...
         os.remove("sound2.wav")
 
 
@@ -56,14 +40,34 @@ def main():
 
 
 '''
-helper functions
+Helper Functions
 '''
 
 
+
+def openfile():
+    # '''functions as "open file..." '''
+    return AudioSegment.from_file(filedialog.askopenfilename(filetypes=[("wav file", ".wav")])) 
+
+def savefile(audio):
+    # '''functions as a "save as..." '''
+    filename = filedialog.asksaveasfile(defaultextension='.wav',filetypes=[("wav file",".wav")])
+    audio.export(filename.name, format="wav")
+
 def harmonize(sound1):
+    # implements simple harmony
+    global harmony_canon
     #shift sound2 by how many semitones user wants
     y, sr = librosa.load("song.wav", sr=16000)
-    shifted = librosa.effects.pitch_shift(y, sr=sr, n_steps=input("shift harmony by _ semitones: "))
+    if harmony_canon in ['c','C']: shifted = librosa.effects.pitch_shift(y, sr=sr, n_steps=-12)
+    else: 
+        #make sure user input is a number
+        while True:
+            try: 
+                semitones = int(input("shift harmony by __ semitones: "))
+                break
+            except (ValueError): print("input a number please ")
+        shifted = librosa.effects.pitch_shift(y, sr=sr, n_steps=semitones)
     #need to turn shifted back to other format
     soundfile.write("sound2.wav", shifted, sr)
     sound2 = AudioSegment.from_file("sound2.wav", format="wav")
@@ -80,7 +84,16 @@ def delay(sound):
     y, sr = librosa.load("song.wav", sr=16000)
     #detect BPM
     bpm = librosa.beat.tempo(y=y, sr=sr)
-    length = 4/bpm*60*1000  #milliseconds
+    while True:
+        try: 
+            beats = input("The imitation melody starts after __ beats: (for default value press ENTER): ")
+            if beats == "":
+                beats = 4
+                break
+            beats = int(beats)
+            break
+        except ValueError: print("beats must be a number")
+    length = beats/bpm*60*1000  #milliseconds
     silence = AudioSegment.silent(duration=length) #append silence to 2nd harmony
     delayed_harm = silence.append(sound)
     return delayed_harm
